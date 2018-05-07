a486e00654d4e001d0e658eb7d16ad7ee1bcce60
Ayush Agarwal <aagarwal9782@gmail.com>
2018-05-08 00:59:57 +0530

    made zipping methods

---
+++ steganographylibrary/src/main/java/com/ayush/steganographylibrary/Stegnography.java
@@ -0,0 +1,48 @@
+package com.ayush.steganographylibrary;
+
+import android.util.Base64;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.util.zip.GZIPInputStream;
+import java.util.zip.GZIPOutputStream;
+
+public class Stegnography {
+
+    String message;
+    String secret_key;
+    String imagePath;
+
+    public Stegnography(String message, String secret_key, String imagePath) {
+        this.message = message;
+        this.secret_key = secret_key;
+        this.imagePath = imagePath;
+    }
+
+    public static byte[] compress(String string) throws IOException {
+        ByteArrayOutputStream os = new ByteArrayOutputStream(string.length());
+        GZIPOutputStream gos = new GZIPOutputStream(os);
+        gos.write(string.getBytes());
+        gos.close();
+        byte[] compressed = os.toByteArray();
+        os.close();
+        return compressed;
+    }
+
+    public static String decompress(byte[] compressed) throws IOException {
+        final int BUFFER_SIZE = 32;
+        ByteArrayInputStream is = new ByteArrayInputStream(compressed);
+        GZIPInputStream gis = new GZIPInputStream(is, BUFFER_SIZE);
+        StringBuilder string = new StringBuilder();
+        byte[] data = new byte[BUFFER_SIZE];
+        int bytesRead;
+        while ((bytesRead = gis.read(data)) != -1) {
+            string.append(new String(data, 0, bytesRead));
+        }
+        gis.close();
+        is.close();
+        return string.toString();
+    }
+}
