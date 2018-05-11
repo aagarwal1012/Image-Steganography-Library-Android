e3de6ee33e1d8e2ab9736de4c1508c6d419bccf4
Ayush Agarwal <aagarwal9782@gmail.com>
2018-05-11 16:12:57 +0530

    open bitmap c++ method added

---
+++ steganographylibrary/src/main/cpp/native-lib.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+
+using namespace std;
+
+
+//reading bitmap
+unsigned char* readBMP(char* filename)
+{
+    int i;
+    FILE* f = fopen(filename, "rb");
+    unsigned char info[54];
+    fread(info, sizeof(unsigned char), 54, f); // read the 54-byte header
+
+    // extract image height and width from header
+    int width = *(int*)&info[18];
+    int height = *(int*)&info[22];
+
+    int size = 3 * width * height;
+    unsigned char* data = new unsigned char[size]; // allocate 3 bytes per pixel
+    fread(data, sizeof(unsigned char), size, f); // read the rest of the data at once
+    fclose(f);
+
+    for(i = 0; i < size; i += 3)
+    {
+        unsigned char tmp = data[i];
+        data[i] = data[i+2];
+        data[i+2] = tmp;
+    }
+
+    return data;
+}
