b2930f42f8ef1beef1ac52cf3854bcb7eb147288
Ayush Agarwal <aagarwal9782@gmail.com>
2018-05-16 14:46:29 +0530

    Steganography class made

--- steganographylibrary/src/main/java/com/ayush/steganographylibrary/Stegnography.java
+++ steganographylibrary/src/main/java/com/ayush/steganographylibrary/Stegnography.java
@@ -1,16 +1,40 @@
 package com.ayush.steganographylibrary;
 
+import java.io.File;
+
 public class Stegnography {
 
     String message;
     String secret_key;
-    String imagePath;
+    File bitmap;
 
-    public Stegnography(String message, String secret_key, String imagePath) {
+    public Stegnography(String message, String secret_key, File bitmap) {
         this.message = message;
         this.secret_key = secret_key;
-        this.imagePath = imagePath;
+        this.bitmap = bitmap;
     }
 
+    public String getMessage() {
+        return message;
+    }
 
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getSecret_key() {
+        return secret_key;
+    }
+
+    public void setSecret_key(String secret_key) {
+        this.secret_key = secret_key;
+    }
+
+    public File getBitmap() {
+        return bitmap;
+    }
+
+    public void setBitmap(File bitmap) {
+        this.bitmap = bitmap;
+    }
 }
