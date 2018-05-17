4b1e20c6b08638c00030862222455b485b1ff775
Ayush Agarwal <aagarwal9782@gmail.com>
2018-05-17 12:34:35 +0530

    added methods in TextDecoding class

--- steganographylibrary/src/main/java/com/ayush/steganographylibrary/Text/TextDecoding.java
+++ steganographylibrary/src/main/java/com/ayush/steganographylibrary/Text/TextDecoding.java
@@ -1,4 +1,77 @@
 package com.ayush.steganographylibrary.Text;
 
-public class TextDecoding {
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
+
+import com.ayush.steganographylibrary.Utils.Utility;
+import com.ayush.steganographylibrary.Utils.Zipping;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+public class TextDecoding extends AsyncTask<File, Void, TextStegnography> {
+
+    private final static String TAG=TextDecoding.class.getName();
+    private Activity activity;
+    private ProgressDialog progressDialog;
+    private boolean isImageDecoded;
+
+    public TextDecoding(Activity activity) {
+        super();
+        this.activity = activity;
+        isImageDecoded = false;
+    }
+
+    @Override
+    protected void onPreExecute() {
+        super.onPreExecute();
+        progressDialog = new ProgressDialog(activity); //Here I get an error: The constructor ProgressDialog(PFragment) is undefined
+        progressDialog.setMessage("Loading, Please Wait...");
+        progressDialog.setTitle("Decoding Message");
+        progressDialog.setIndeterminate(true);
+        progressDialog.setCancelable(false);
+    }
+
+    @Override
+    protected void onPostExecute(TextStegnography textStegnography) {
+        super.onPostExecute(textStegnography);
+        progressDialog.dismiss();
+    }
+
+    @Override
+    protected void onProgressUpdate(Void... values) {
+        super.onProgressUpdate(values);
+        progressDialog.show();
+    }
+
+    @Override
+    protected TextStegnography doInBackground(File... files) {
+        TextStegnography result = null;
+        publishProgress();
+        Bitmap bitmap = BitmapFactory.decodeFile(files[0].getAbsolutePath());
+        if (bitmap == null)
+            return result;
+        List<Bitmap> srcEncodedList = Utility.splitImage(bitmap);
+        String decoded = EncodeDecode.decodeMessage(srcEncodedList);
+
+        //free memory
+        for (Bitmap bitm : srcEncodedList)
+            bitm.recycle();
+
+        if (Utility.isStringEmpty(decoded)) {
+            try {
+                isImageDecoded = true;
+                result.setEncrypted_zip(decoded.getBytes());
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+
+        return result;
+    }
 }
