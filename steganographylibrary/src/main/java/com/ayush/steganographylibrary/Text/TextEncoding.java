aa1ea936a9356a62b78b8b4540fba0de597e80d1
Ayush Agarwal <aagarwal9782@gmail.com>
2018-05-18 20:40:46 +0530

    added documentation for TextEncoding class and fix some problems

--- steganographylibrary/src/main/java/com/ayush/steganographylibrary/Text/TextEncoding.java
+++ steganographylibrary/src/main/java/com/ayush/steganographylibrary/Text/TextEncoding.java
@@ -71,6 +71,7 @@ public class TextEncoding extends AsyncTask<TextSteganography, Integer, TextSteg
     @Override
     protected TextSteganography doInBackground(TextSteganography... textSteganographies) {
 
+        //making result object
         TextSteganography result = null;
 
         maximumProgress = 0;
@@ -79,33 +80,44 @@ public class TextEncoding extends AsyncTask<TextSteganography, Integer, TextSteg
 
             TextSteganography textStegnography = textSteganographies[0];
 
-            //Encrypt
+            //Encrypting and compressing the message with the secret key
             if (Utility.isStringEmpty(textStegnography.getSecret_key()))
             {
                 try{
                     Crypto encryption = new Crypto(textStegnography.getMessage(), textStegnography.getSecret_key());
                     textStegnography.setEncrypted_zip(encryption.getEncrypted_zip());
+
+                    //free encryption
+                    encryption = null;
+
                 }catch (Exception e){
                     e.printStackTrace();
                 }
             }
 
+            //calculating square blocks needed
             int pixels_needed = EncodeDecode.numberOfPixelForMessage(textStegnography.getEncrypted_zip().toString());
             int square_blocks_needed = Utility.squareBlockNeeded(pixels_needed);
 
+            //making bitmap factory options for decoding file
             BitmapFactory.Options options = new BitmapFactory.Options();
             options.inJustDecodeBounds = true;
             int sample = calculateInSampleSize(options, square_blocks_needed * Utility.SQUARE_BLOCK_SIZE, square_blocks_needed * Utility.SQUARE_BLOCK_SIZE);
             options.inJustDecodeBounds = false;
             options.inSampleSize = sample;
 
+            //decoding image file to bitmap
             Bitmap bitmap = BitmapFactory.decodeFile(textStegnography.getBitmap().getAbsolutePath(), options);
             int originalHeight = bitmap.getHeight();
             int originalWidth = bitmap.getWidth();
 
-            List<Bitmap> srclist = Utility.splitImage(bitmap);
+            //splitting bitmap
+            List<Bitmap> src_list = Utility.splitImage(bitmap);
 
-            List<Bitmap> encodedList = EncodeDecode.encodeMessage(srclist, textStegnography.getEncrypted_zip().toString(), new EncodeDecode.ProgressHandler() {
+            //encoding encrypted compressed message into image
+            List<Bitmap> encoded_list = EncodeDecode.encodeMessage(src_list, textStegnography.getEncrypted_zip().toString(), new EncodeDecode.ProgressHandler() {
+
+                //Progress Handler
                 @Override
                 public void setTotal(int tot) {
                     maximumProgress = tot;
@@ -128,37 +140,43 @@ public class TextEncoding extends AsyncTask<TextSteganography, Integer, TextSteg
 
             //free Memory
             bitmap.recycle();
-            for (Bitmap bitm : srclist)
+            for (Bitmap bitm : src_list)
                 bitm.recycle();
 
+            //Java Garbage collector
             System.gc();
 
-
-            Bitmap srcEncoded = Utility.mergeImage(encodedList, originalHeight, originalWidth);
+            //merging the split encoded image
+            Bitmap srcEncoded = Utility.mergeImage(encoded_list, originalHeight, originalWidth);
             result.setEncrypted_image(bitmap);
-
-
         }
 
         return result;
     }
 
+    /**
+     * This method is used to calculate sample size.
+     * @parameter : options {BitmapFactory options}
+     * @parameter : required_width {Integer}
+     * @parameter : required_height {Integer}
+     * @return : inSamplesize {Integer}
+     */
     public static int calculateInSampleSize(
-            BitmapFactory.Options options, int reqWidth, int reqHeight) {
+            BitmapFactory.Options options, int required_width, int required_height) {
         // Raw height and width of image
         final int height = options.outHeight;
         final int width = options.outWidth;
         int inSampleSize = 1;
 
-        if (height > reqHeight || width > reqWidth) {
+        if (height > required_height || width > required_width) {
 
             final int halfHeight = height / 2;
             final int halfWidth = width / 2;
 
             // Calculate the largest inSampleSize value that is a power of 2 and keeps both
             // height and width larger than the requested height and width.
-            while ((halfHeight / inSampleSize) > reqHeight
-                    && (halfWidth / inSampleSize) > reqWidth) {
+            while ((halfHeight / inSampleSize) > required_height
+                    && (halfWidth / inSampleSize) > required_width) {
                 inSampleSize *= 2;
             }
         }
